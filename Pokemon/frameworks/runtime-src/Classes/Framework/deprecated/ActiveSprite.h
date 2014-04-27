ae3a1da1e90c209007dcc5e11fcb4e13b4f1529c
M.Wan <wan_maple@hotmail.com>
2014-04-27 18:17:18 +0800

    add deprecated folder

---
+++ Pokemon/frameworks/runtime-src/Classes/Framework/deprecated/ActiveSprite.h
@@ -0,0 +1,34 @@
+/******************************
+Description: ActiveSprite, extends EventNode
+Author: M.Wan
+Date: 4/22/2014
+******************************/
+
+#ifndef __UI_ACTIVESPRITE__
+#define __UI_ACTIVESPRITE__
+
+#include "EventNode.h"
+
+namespace framework
+{
+
+	class ActiveSprite : public EventNode
+	{
+	public:
+		static ActiveSprite *create(const char *szSpritePath);
+		static ActiveSprite *create(cocos2d::Node *pSprite);
+		static ActiveSprite *createWithCallback(const char *szSpritePath, const TouchCallback &touchCallback, const KeyboardCallback &keyboardCallback);
+		static ActiveSprite *createWithCallback(cocos2d::Node *pSprite, const TouchCallback &touchCallback, const KeyboardCallback &keyboardCallback);
+
+		void focus() override;
+		void blur() override;
+	protected:
+		virtual ~ActiveSprite();
+
+		bool initWithSprite(cocos2d::Node *pSprite, const TouchCallback &touchCallback, const KeyboardCallback &keyboardCallback);
+		void updateImageVisibility() override;
+	};
+
+}
+
+#endif
