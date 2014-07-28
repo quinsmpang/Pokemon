1b88a426eae221e439ef82e939eb251beb5587f1
Wan, Maple <wan_maple@hotmail.com>
2014-07-27 21:43:55 -0700

    fix export tool bug

--- Pokemon/src/utils/DataCenter.lua
+++ Pokemon/src/utils/DataCenter.lua
@@ -7,3 +7,5 @@
 DataCenter = {}
 
 DataCenter.currentPlayerData = nil		-- 当前玩家数据(PlayerData)
+DataCenter.currentPetData = nil		-- 当前拥有的宠物数据({Pet})
+DataCenter.currentBagData = nil		-- 当前背包数据({Item})
