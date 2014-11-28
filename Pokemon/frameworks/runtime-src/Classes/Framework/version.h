8ad34e61222bf03c2838e79ea618ff5888855796
Wan, Maple <wan_maple@hotmail.com>
2014-11-29 01:12:24 +0800

    update version info

--- Pokemon/frameworks/runtime-src/Classes/Framework/version.h
+++ Pokemon/frameworks/runtime-src/Classes/Framework/version.h
@@ -4,20 +4,21 @@
 /***********************************************
 UPDATE LOG:
 v1.00	Add cocos2d-x lua based essential components to the framework
-v1.01		Add some UI and utilities for the project
+v1.01	Add some UI and utilities for the project
 v1.02	Add some encrytion mechanism to the framework
 v1.03	Add ListMenu to the framework
 v1.04	Add ImageUtils to deal with gif images
 v1.05	Add ZipHelper to handle zip compression
 v1.06	Add win32 keyboard support into the framework
 v1.07	Add AES encryption and standard save data support
+v1.08	Add TitleSwitch control to the framework
 ***********************************************/
 
 #include <iostream>
 #include "tolua_fix.h"
 
-static const char *VERSION = "v1.07";
-static float VERSION_NUM = 1.07;
+static const char *VERSION = "v1.08";
+static float VERSION_NUM = 1.08;
 
 void printVersion()
 {
