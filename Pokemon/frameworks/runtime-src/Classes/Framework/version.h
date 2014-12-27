503d2a82c3f67ad39d8a523013fb90d5b9e2b17f
M.Wan <wan_maple@hotmail.com>
2014-12-27 13:43:39 +0800

    tmp

--- Pokemon/frameworks/runtime-src/Classes/Framework/version.h
+++ Pokemon/frameworks/runtime-src/Classes/Framework/version.h
@@ -13,8 +13,8 @@ v1.06	Add win32 keyboard support into the framework
 v1.07	Add AES encryption and standard save data support
 v1.08	Add TitleSwitch control to the framework
 v1.09	Integrate lanes lib to the framework
-v1.10 Add gif decode method in ohter platform excluding win32, fix zip on ios issue.
-v1.11 Add simple threading to the framework, threading features will be expanded in the future.
+v1.10	Add gif decode method in ohter platform excluding win32, fix zip on ios issue.
+v1.11	Add simple threading to the framework, threading features will be expanded in the future.
 ***********************************************/
 
 #include <iostream>
