aa860031c55d15af652821ef8c45b1b1b7122e7f
Wan, Maple <wan_maple@hotmail.com>
2015-01-07 03:04:09 +0800

    battle in process

--- Pokemon/src/scene/battle/BattleUIController.lua
+++ Pokemon/src/scene/battle/BattleUIController.lua
@@ -44,8 +44,26 @@ function BattleUIController:renderView()
 	bg:setPosition(winSize.width * 0.5, winSize.height * 0.5)
 	self.root:addChild(bg)
 
+	local fieldFrameName = "images/battle/battle_field" .. fieldType .. ".png"
+	local fieldPlayer = cc.Sprite:createWithSpriteFrameName(fieldFrameName)
+	fieldPlayer:setPosition(winSize.width * 1.2, winSize.height * 0.3)
+	self.root:addChild(fieldPlayer)
+	local fieldEnemy = cc.Sprite:createWithSpriteFrameName(fieldFrameName)
+	fieldEnemy:setPosition(winSize.width * -0.2, winSize.height * 0.7)
+	self.root:addChild(fieldEnemy)
 	if battleType == Enumerations.BATTLE_TYPE.WILD then
+		-- 野生精灵对战 直接显示野生精灵
 		local pokemonId = self:getScene():getIntAttribute("pokemon_id")
 		local pokemonLevel = self:getScene():getIntAttribute("pokemon_level")
+
+		-- 随机生成神奇宝贝
+		local pokemonModel = Pokemon:create(pokemonId, pokemonLevel)
+	elseif battleType == Enumerations.BATTLE_TYPE.TRAINER_1V1 then
+		-- 训练师对战1v1
+		local trainerId = self:getScene():getIntAttribute("trainer_id")
+	elseif battleType == Enumerations.BATTLE_TYPE.TRAINER_2V2 then
+		-- 训练师对战2v2
+		local trainer1Id = self:getScene():getIntAttribute("trainer1_id")
+		local trainer2Id = self:getScene():getIntAttribute("trainer2_id")
 	end
 end
